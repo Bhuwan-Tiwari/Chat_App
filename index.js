fd312454d6b8ecbe12beb5e51b1cb79246d1bdac
Bhuwan Tiwari <bhuwan1200@gmail.com>
2024-03-04 19:01:20 +0530

    Added codes from client to server to all the client

--- index.js
+++ index.js
@@ -7,24 +7,20 @@ const server = http.createServer(app);
 const io = socketio(server)
 
 io.on('connection', (socket) => {  //when any of the machine emmit a connection event then other one recieves by using io.on  //its ecpect a event called connection 
-    console.log('a user connected',socket.id);  //each socket object has unique id :-socket.id
+  console.log('a user connected', socket.id);  //each socket object has unique id :-socket.id
 
-    socket.on('from_client',()=>
-    {
-      console.log('event comming from client')
-    })
-    setInterval(()=>
-    {
-         socket.emit('from_server')   //after every two secound a mssg is emmiting from server
-    },2000)
+  socket.on('msg_send', (data) =>//server receive data
+  {
+    console.log(data)  // data is then printed on server side
+    io.emit('msg_rcvd', data)  //now the server emit this data to all client connected through socket
+  })
 
-  });
-app.use('/',  express.static(__dirname+'/public'))
- 
+});
+app.use('/', express.static(__dirname + '/public'))
 
-server.listen(3000,()=>
-{
-    console.log('server started')
+
+server.listen(3000, () => {
+  console.log('server started')
 })
 
 //listening or receving an event using socket.on and emmiting and event using socket.emmit
--- public/script.js
+++ public/script.js
@@ -1,19 +1,23 @@
 //script.js js use to write js code in frontend
 let btn = document.getElementById('btn')
+let inputMsg = document.getElementById('newmsg')
+let msgList = document.getElementById('msglist')
 
 var socket = io()  //its started a connection event
 //from any of the machine lets browser create websocket object then a new websocket connection is establish in backet server recive that connection usid io.on
 //we make a socket connection here  
 
 btn.onclick = function exec() {
-    socket.emit('from_client')
+    socket.emit('msg_send',
+           {
+             msg: inputMsg.value
+            })
 }
 
-socket.on('from_server', ()=>
+
+socket.on('msg_rcvd',(data)=>
 {
-    console.log("collect a new event from server")
-    const div = document.createElement('div')
-    div.innerText = 'new event from server'
-    console.log(div)
-    document.body.appendChild(div)
-})     
+    let limsg = document.createElement('li')
+    limsg.innerText=  data.msg;
+    msgList.appendChild(limsg)
+})
